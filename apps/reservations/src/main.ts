e04889ce5e4834f07909bf8ba7dc21784b0f6017
Raza Shihab Mahbub <socar@SOCARs-MacBook-Pro.local>
2023-08-07 15:51:12 +0800

    created generic repository and added reservation controllers and services using nest cli

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -4,5 +4,6 @@ import { ReservationsModule } from './reservations.module';
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
   await app.listen(3000);
+  console.log('Reservation Service is running on port 3000')
 }
 bootstrap();
