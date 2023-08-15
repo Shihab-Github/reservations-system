b7b28523ca58b6ef0bfe6a0ce797d3602885bc04
Raza Shihab Mahbub <socar@SOCARs-MacBook-Pro.local>
2023-08-15 11:06:09 +0800

    dockerized reservations app

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationsModule } from './reservations.module';
+import { ValidationPipe } from '@nestjs/common';
+import {Logger} from 'nestjs-pino'
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
+  app.useLogger(app.get(Logger))
   await app.listen(3000);
   console.log('Reservation Service is running on port 3000')
 }
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -4,9 +4,10 @@ import { ReservationsController } from './reservations.controller';
 import { DatabaseModule } from '@app/common/database/database.module';
 import { ReservationsRepository } from './reservations.repository';
 import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
+import { LoggerModule } from '@app/common/logger/logger.module';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([{name: ReservationDocument.name, schema: ReservationSchema}])],
+  imports: [DatabaseModule, LoggerModule, DatabaseModule.forFeature([{name: ReservationDocument.name, schema: ReservationSchema}])],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository]
 })
